eb6975eb41b2b69a3bc0ab4c6b0c3ab1b0a8871b
woopzz <bkovtunovych@gmail.com>
2025-10-01 17:29:19 +0300

    Add starlette_exporter.

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from app.core.config import settings
 from app.router import router
@@ -8,6 +9,8 @@ app = FastAPI(
     summary='Manage users. Handle authentication.',
     version='1.0.0',
 )
+app.add_middleware(PrometheusMiddleware, skip_paths=['/metrics'])
+app.add_route('/metrics', handle_metrics)
 app.include_router(router, prefix=settings.API_V1_STR)
 
 
